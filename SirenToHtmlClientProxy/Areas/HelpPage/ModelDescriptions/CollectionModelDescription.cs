3543dd30829266f50a5e9baa1c1d818d6dd3a981
mcintyre321 <mcintyre321@gmail.com>
2016-07-14 13:18:38 +0100

    Add project files.

---
+++ SirenToHtmlClientProxy/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace SirenToHtmlClientProxy.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ SirenToHtmlClientProxy/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace SirenToHtmlClientProxy.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
