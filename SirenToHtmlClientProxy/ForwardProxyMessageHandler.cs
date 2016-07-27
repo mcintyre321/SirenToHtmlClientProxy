d532b380ee8cfe5c95e2273a1c6aa197d84a7546
Harry McIntyre <Harry.McIntyre@totaljobsgroup.com>
2016-07-27 18:35:22 +0100

    Some updates

--- SirenToHtmlClientProxy/ForwardProxyMessageHandler.cs
+++ SirenToHtmlClientProxy/ForwardProxyMessageHandler.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -12,7 +13,12 @@ using System.Xml.Linq;
 using FormFactory;
 using FormFactory.RazorEngine;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using OneOf;
+using RazorEngine.Text;
+using SirenSharp;
+using Action = SirenSharp.Action;
+using Exception = System.Exception;
 
 namespace SirenToHtmlClientProxy
 {
@@ -34,7 +40,10 @@ namespace SirenToHtmlClientProxy
             if (request.Method == HttpMethod.Get || request.Method == HttpMethod.Trace) request.Content = null;
 
             var first = new Uri(destinations.First());
-            var uri = new UriBuilder(first);
+
+            Func<string, string> sanitiseUrls = s => s.Replace(first.ToString(), "/");
+
+            var uri = new UriBuilder(first + request.RequestUri.PathAndQuery);
 
             uri.Host = first.Host;
             uri.Port = first.Port;
@@ -46,9 +55,17 @@ namespace SirenToHtmlClientProxy
             request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.siren+json"));
 
             request.Headers.Host = uri.Host;
-            request.RequestUri = new Uri(uri.ToString());
             request.Headers.AcceptEncoding.Clear();
- 
+
+            var qs = request.RequestUri.ParseQueryString();
+
+            var methodOverride = qs["_method"];
+            if (methodOverride != null)
+            {
+                request.Method = new HttpMethod(methodOverride);
+                request.RequestUri = new Uri(uri.ToString().Replace("_method=" + methodOverride, ""));
+            }
+
             var responseMessage =
                 await
                     new HttpClient().SendAsync(request, HttpCompletionOption.ResponseHeadersRead,
@@ -61,64 +78,137 @@ namespace SirenToHtmlClientProxy
             {
                 try
                 {
-                    var content = await responseMessage.Content.ReadAsStringAsync();
-                    content = content.Replace(first.ToString(), "/");
-                    var sirenObject = JsonConvert.DeserializeObject<SirenSharp.Entity>(content);
-                    var htmlModel = new List<OneOf<PropertyVm, FormVm>>();
-                    foreach (var property in sirenObject.Properties)
+                    if (request.Method == HttpMethod.Post)
                     {
-                        htmlModel.Add(new PropertyVm(typeof(string), property.Key)
+                        if (responseMessage.StatusCode == HttpStatusCode.Created
+                            || responseMessage.StatusCode == HttpStatusCode.Accepted)
                         {
-                            Value = property.Value.ToString(),
-                            Readonly = true,
-                            DisplayName = property.Key
-                        });
-                    }
-                    foreach (var link in sirenObject.Links)
-                    {
-                        var element = new XElement("a", new XAttribute("href", link.Href));
-                        var name = string.Join(", ", link.Rel);
-                        htmlModel.Add(new PropertyVm(typeof(XElement), name)
-                        {
-                            Value = element,
-                            Readonly = true,
-                            DisplayName = name
-                        });
-                    }
-                    foreach (var action in sirenObject.Actions)
-                    {
-                        var form = new FormVm
-                        {
-                            ActionUrl = action.Href.ToString(),
-                            DisplayName = action.Title ?? action.Name,
-                            Method = action.Method.ToString(),
-                            Inputs = action.Fields.Select(field =>
+                            if (responseMessage.Headers.Location != null)
                             {
-                                var property = new PropertyVm(typeof(string), field.Name) {DisplayName = field.Name};
-                                return property;
-                            })
-                        };
-                        htmlModel.Add(form);
+                                var redirectMessage = request.CreateResponse(HttpStatusCode.Moved);
+                                redirectMessage.Headers.Location =
+                                    new Uri(sanitiseUrls(responseMessage.Headers.Location.ToString()), UriKind.Relative);
+                                return redirectMessage;
+                            }
+                        }
+                        if (responseMessage.StatusCode == HttpStatusCode.OK)
+                        {
+                            //fall through to get handler
+                        }
+                        else
+                        {
+                            throw new NotImplementedException();
+                        }
                     }
-                     
-                    var razorHelper = new FormFactory.RazorEngine.RazorTemplateHtmlHelper();
-                    var htmls = htmlModel
-                        .Select(x => x.Match(
-                            propertyVm => propertyVm.Render(razorHelper),
-                            formVm => formVm.Render(razorHelper)
-                            )
-                        )
-                        .Select(htmlStr => htmlStr.ToString());
-                    responseMessage.Content = new StringContent(string.Join("", htmls), Encoding.Default, "text/html");
+                    var content = await responseMessage.Content.ReadAsStringAsync();
+                    var htmls = ReadSirenAndConvertToForm(sanitiseUrls(content));
+                    var stringContent = new StringContent(htmls, Encoding.Default, "text/html");
+                    responseMessage.Content = stringContent;
                 }
                 catch (Exception ex)
                 {
-                    responseMessage.Content = new StringContent(request.RequestUri.ToString() + request.Headers.ToString()+ ex.ToString());
+                    responseMessage.Content =
+                        new StringContent(request.RequestUri.ToString() + request.Headers.ToString() + ex.ToString());
                 }
             }
 
             return responseMessage;
         }
 
+        private static string ReadSirenAndConvertToForm(string content)
+        {
+            var entity = JsonConvert.DeserializeObject<SirenSharp.Entity>(content);
+            var list = new List<OneOf<PropertyVm, FormVm>>();
+
+            entity.Links?.Select(BuildPropertyVmFromLink).ToList().ForEach(x => list.Add(x));
+            entity.Properties?.Select(PropertyVmFromJToken).ToList().ForEach(x => list.Add(x));
+            entity.Actions?.Select(BuildFormVmFromAction).ToList().ForEach(x => list.Add(x));
+            entity.Entities?.Select(BuildPropertyVmFromSubEntity).ToList().ForEach(x => list.AddRange(x));
+
+            var razorHelper = new FormFactory.RazorEngine.RazorTemplateHtmlHelper();
+            var elements = list
+                .Select(x => x.Match(
+                    propertyVm => propertyVm.Render(razorHelper),
+                    formVm => ((FormVm) formVm).Render(razorHelper)
+                    )
+                ).ToList();
+
+
+            var sirenElement = new PropertyVm(typeof(string), "_response")
+            {
+                Readonly = true,
+                DisplayName = "Siren Response",
+                GetCustomAttributes =  () => new object[] { new DataTypeAttribute(DataType.MultilineText) },
+                Value = JToken.Parse(content).ToString(Formatting.Indented)
+            };
+
+            elements.Add(sirenElement.Render(razorHelper));
+
+            return string.Join("", elements.Select(htmlStr => htmlStr.ToString()));
+        }
+
+        private static IEnumerable<OneOf<PropertyVm, FormVm>> BuildPropertyVmFromSubEntity(
+            OneOf<SubEntity, SubEntityLink> e)
+        {
+            return e.Match(
+                subEntity =>
+                {
+                    var list = new List<OneOf<PropertyVm, FormVm>>();
+                    subEntity.Links?.Select(BuildPropertyVmFromLink).ToList().ForEach(x => list.Add(x));
+                    subEntity.Properties?.Select(PropertyVmFromJToken).ToList().ForEach(x => list.Add(x));
+                    subEntity.Actions?.Select(BuildFormVmFromAction).ToList().ForEach(x => list.Add(x));
+                    subEntity.Entities?.Select(BuildPropertyVmFromSubEntity).ToList().ForEach(x => list.AddRange(x));
+                    return list.AsEnumerable();
+                },
+                subEntity => Enumerable.Empty<OneOf<PropertyVm, FormVm>>().AsEnumerable()
+            );
+        }
+
+        private static PropertyVm PropertyVmFromJToken(KeyValuePair<string, JToken> property)
+        {
+            var propertyVm = new PropertyVm(typeof(string), property.Key)
+            {
+                Value = property.Value.ToString(),
+                Readonly = true,
+                DisplayName = property.Key
+            };
+            propertyVm.GetCustomAttributes = () => new object[] {new DataTypeAttribute(DataType.MultilineText)};
+            return propertyVm;
+        }
+
+        private static PropertyVm BuildPropertyVmFromLink(Link link)
+        {
+            var element = new XElement("a", new XAttribute("href", link.Href));
+            var name = string.Join(", ", link.Rel);
+
+            element.Value = name;
+            var propertyVm = new PropertyVm(typeof(XElement), name)
+            {
+                Value = element,
+                Readonly = true,
+                DisplayName = name,
+                Name = name
+            };
+            return propertyVm;
+        }
+
+        private static FormVm BuildFormVmFromAction(Action action)
+        {
+            var form = new FormVm
+            {
+                ActionUrl = action.Href.ToString(),
+                DisplayName = action.Title ?? action.Name ?? "link",
+                Method = action?.Method.ToString().ToLower(),
+                Inputs = action.Fields?.Select(field => new PropertyVm(typeof(string), field.Name) {DisplayName = field.Name})?.ToArray() ?? Enumerable.Empty<PropertyVm>()
+            };
+
+            if (form.Method != "GET" && form.Method != "post")
+            {
+                form.ActionUrl += form.ActionUrl.Contains("?") ? "&" : "?";
+                form.ActionUrl += "_method=" + form.Method.ToString().ToUpper();
+                form.Method = "post";
+            }
+            return form;
+        }
     }
 }
